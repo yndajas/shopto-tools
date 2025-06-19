ca2093c4e0be1c08c5517e8986cf19bb483754b6
Ynda Jas <yndajas@gmail.com>
2025-06-19 21:27:11 +0100

    Implement better printing and use classes
    I tried a console.table along the way with either numeric or string
    values, but it didn't quite get me where I wanted to be with
    (right-aligned table columns; currency and percentage symbols without
    string quotes; no useless index)

--- psnCardsByDiscountValue.js
+++ psnCardsByDiscountValue.js
@@ -1,43 +1,123 @@
-function moneyStringToMoney(string) {
-  return Number.parseFloat(string.replace("£", ""));
+class Money {
+  number;
+  string;
+
+  constructor({ number, string } = {}) {
+    if (number) {
+      this.number = number;
+      this.string = new Intl.NumberFormat("en-GB", {
+        style: "currency",
+        currency: "GBP",
+      }).format(number);
+    } else {
+      this.string = string;
+      this.number = Number.parseFloat(string.replace("£", ""));
+    }
+  }
 }
 
-function discountPercentage(value, price) {
-  const decimal = 1 - price / value;
-  return Number.parseFloat((decimal * 100).toFixed(2));
+class Discount {
+  number;
+  string;
+
+  constructor(price, value) {
+    this.number = 1 - price.number / value.number;
+    this.string = new Intl.NumberFormat("en-GB", {
+      style: "percent",
+      minimumFractionDigits: 2,
+    }).format(this.number);
+  }
 }
 
-const cards = Array.from(document.querySelectorAll(".itemlist__info")).map(
-  (cardElement) => {
-    const value = moneyStringToMoney(
-      cardElement.querySelector(".cross_price").innerText,
-    );
+class Card {
+  value;
+  basePrice;
+  silverPrice;
+  goldPrice;
+  #_baseDiscount;
+  #_silverDiscount;
+  #_goldDiscount;
+
+  constructor(value, basePrice, silverPrice, goldPrice) {
+    this.value = value;
+    this.basePrice = basePrice;
+    this.silverPrice = silverPrice;
+    this.goldPrice = goldPrice;
+  }
+
+  static fromHtmlElement(cardElement) {
+    const value = new Money({
+      string: cardElement.querySelector(".cross_price").innerText,
+    });
 
     const [goldPrice, silverPrice] = Array.from(
       cardElement.querySelectorAll(".memb-badge-container"),
-    ).map((element) => moneyStringToMoney(element.innerText));
+    ).map((element) => new Money({ string: element.innerText }));
 
-    const basePrice = Number.parseFloat(((silverPrice / 99) * 100).toFixed(2));
+    const basePrice = new Money({
+      number: Number.parseFloat(((silverPrice.number / 99) * 100).toFixed(2)),
+    });
 
-    return {
-      value,
-      base: {
-        price: basePrice,
-        discount: discountPercentage(value, basePrice),
-      },
-      silver: {
-        price: silverPrice,
-        discount: discountPercentage(value, silverPrice),
-      },
-      gold: {
-        price: goldPrice,
-        discount: discountPercentage(value, goldPrice),
-      },
-    };
-  },
-);
+    return new Card(value, basePrice, silverPrice, goldPrice);
+  }
 
-console.log(
-  "cards sorted by discount value",
-  cards.sort((a, b) => b.base.discount - a.base.discount),
-);
+  baseDiscount() {
+    this._baseDiscount ||= new Discount(this.basePrice, this.value);
+    return this._baseDiscount;
+  }
+
+  silverDiscount() {
+    this._silverDiscount ||= new Discount(this.basePrice, this.value);
+    return this._silverDiscount;
+  }
+
+  goldDiscount() {
+    this._goldDiscount ||= new Discount(this.basePrice, this.value);
+    return this._goldDiscount;
+  }
+
+  print({ extended } = { extended: false }) {
+    const logArgs = [
+      "",
+      this.value.string.replace(".00", "").padStart(4, " "),
+      "      ",
+      this.baseDiscount().string.padStart(6, " "),
+      "  ",
+      this.basePrice.string.padStart(7, " "),
+      "        ",
+    ];
+
+    if (extended) {
+      logArgs.push(
+        this.silverDiscount().string.padStart(6, " "),
+        "    ",
+        this.silverPrice.string.padStart(7, " "),
+        "      ",
+        this.goldDiscount().string.padStart(6, " "),
+        "  ",
+        this.goldPrice.string.padStart(7, " "),
+      );
+    }
+
+    console.log(...logArgs);
+  }
+}
+
+function printDiscounts({ extended } = { extended: false }) {
+  if (extended) {
+    console.log(
+      "value  baseDiscount  basePrice  silverDiscount  silverPrice  goldDiscount  goldPrice",
+    );
+  } else {
+    console.log("value  baseDiscount  basePrice");
+  }
+
+  Array.from(document.querySelectorAll(".itemlist__info"))
+    .map(Card.fromHtmlElement)
+    .sort((a, b) => b.baseDiscount().number - a.baseDiscount().number)
+    .forEach((card) => card.print({ extended }));
+}
+
+console.log(`run one of:
+$ printDiscounts()
+$ printDiscounts({ extended: true })`);
